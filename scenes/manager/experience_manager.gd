b8b2c2e7850e51dca44df5af16212740ab92af7d
ahmet <ahmetdrsy@gmail.com>
2023-12-01 08:18:31 +0300

    added health and vial drop component

---
+++ scenes/manager/experience_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var current_experience=0
+
+
+func _ready():
+	GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
+
+
+
+func increment_experience(number: float):
+	current_experience +=number
+	print(current_experience)
+	
+func on_experience_vial_collected(number:float):
+	current_experience += number
+	print(current_experience)
